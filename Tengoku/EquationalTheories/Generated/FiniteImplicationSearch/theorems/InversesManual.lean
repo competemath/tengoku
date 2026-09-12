51eab89546cf8f94cffb136cea80f0a0d740dff1
mikaelbashir <bashirm-y16@mgs.org>
2026-09-12 20:33:45 +0100

    Stage equational-theories/Finite.Equation1447_implies_Equation1431 (#22079)

--- Tengoku/EquationalTheories/Generated/FiniteImplicationSearch/theorems/InversesManual.lean
+++ Tengoku/EquationalTheories/Generated/FiniteImplicationSearch/theorems/InversesManual.lean
@@ -1,4 +1,4 @@
--- Tengoku.EquationalTheories.Generated.FiniteImplicationSearch.theorems.InversesManual: verified translations of equational_theories/Generated/FiniteImplicationSearch/theorems/InversesManual.lean (1 theorem)
+-- Tengoku.EquationalTheories.Generated.FiniteImplicationSearch.theorems.InversesManual: verified translations of equational_theories/Generated/FiniteImplicationSearch/theorems/InversesManual.lean (2 theorems)
 import Tengoku.EquationalTheories.Deps.Superposition
 import Tengoku.Data.Set.Finite.Basic
 import Tengoku.Tactic.TypeStar
@@ -60,5 +60,29 @@ theorem _root_.Finite.Equation1443_implies_Equation1630 (G : Type*) [Magma G] [F
     simp [S]
   intro x y
   simp only [eq1443_implies_eq21374, eq21374_implies_eq1630]
+
+theorem _root_.Finite.Equation1447_implies_Equation1431 (G : Type*) [Magma G] [Finite G] (h : Equation1447 G) : Equation1431 G
+:= by
+  have eq1447_implies_eq21413 : ∀ X Y : G, X = ((X ◇ (Y ◇ X)) ◇ (X ◇ (Y ◇ X))) :=
+    fun X Y => h X (Y ◇ X) Y
+  have eq21413_implies_eq1431 (X Y : G) : ((X ◇ X) ◇ (Y ◇ (X ◇ X))) = X := by
+    let S : Set G := Set.univ
+    have m1 : S.MapsTo (fun s => (s ◇ (Y ◇ s))) S := by
+      intro
+      simp [S]
+    have m2 : S.MapsTo (fun s => (s ◇ s)) S := by
+      intro
+      simp [S]
+    have linv : S.LeftInvOn (fun s => (s ◇ s)) (fun s => (s ◇ (Y ◇ s))) := by
+      intro a ha
+      simp only
+      exact (eq1447_implies_eq21413 a Y).symm
+    have t := linv.surjOn m1
+    rw [Set.Finite.surjOn_iff_bijOn_of_mapsTo (Set.toFinite _) m2] at t
+    have rinv := Set.InjOn.rightInvOn_of_leftInvOn t.injOn linv m2 m1
+    apply rinv _
+    simp [S]
+  intro x y
+  simp only [eq1447_implies_eq21413, eq21413_implies_eq1431]
 
 end EquationalTheories
