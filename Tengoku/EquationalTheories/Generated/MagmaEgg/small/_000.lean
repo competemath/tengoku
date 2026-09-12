2f976a63dbd264b298dba3277da9046f6e7d5f88
mikaelbashir <bashirm-y16@mgs.org>
2026-09-12 20:42:24 +0100

    Stage equational-theories/Equation692_implies_Equation1358 (#15271)

--- Tengoku/EquationalTheories/Generated/MagmaEgg/small/_000.lean
+++ Tengoku/EquationalTheories/Generated/MagmaEgg/small/_000.lean
@@ -1,4 +1,4 @@
--- Tengoku.EquationalTheories.Generated.MagmaEgg.small._000: verified translations of equational_theories/Generated/MagmaEgg/small/_000.lean (1 theorem)
+-- Tengoku.EquationalTheories.Generated.MagmaEgg.small._000: verified translations of equational_theories/Generated/MagmaEgg/small/_000.lean (2 theorems)
 import Tengoku.EquationalTheories.Deps.Magma
 import Tengoku.EquationalTheories.Deps
 
@@ -17,5 +17,10 @@ private abbrev C := @congr_op
 theorem Equation3715_implies_Equation4470 (G: Type _) [Magma G] (h: Equation3715 G) : Equation4470 G
 := fun x y =>
   T (T (h x (M y y)) (C (h x x) (S (h y y)))) (S (h (M x x) y))
+
+theorem Equation692_implies_Equation1358 (G: Type _) [Magma G] (h: Equation692 G) : Equation1358 G
+:= fun x y z =>
+  let v0 := M (M z x) z
+  T (h x y v0) (C (R y) (S (h (M v0 y) x z)))
 
 end EquationalTheories
