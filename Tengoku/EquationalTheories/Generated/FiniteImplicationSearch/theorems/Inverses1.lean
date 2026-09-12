830ad5317f96e2e529f8c487e968f398e7ffaaba
mikaelbashir <bashirm-y16@mgs.org>
2026-09-12 19:07:18 +0100

    Stage equational-theories/Finite.Equation680_implies_Equation1629 (#22058)

--- Tengoku/EquationalTheories/Generated/FiniteImplicationSearch/theorems/Inverses1.lean
+++ Tengoku/EquationalTheories/Generated/FiniteImplicationSearch/theorems/Inverses1.lean
@@ -1,4 +1,4 @@
--- Tengoku.EquationalTheories.Generated.FiniteImplicationSearch.theorems.Inverses1: verified translations of equational_theories/Generated/FiniteImplicationSearch/theorems/Inverses1.lean (56 theorems)
+-- Tengoku.EquationalTheories.Generated.FiniteImplicationSearch.theorems.Inverses1: verified translations of equational_theories/Generated/FiniteImplicationSearch/theorems/Inverses1.lean (57 theorems)
 import Tengoku.EquationalTheories.Deps.Superposition
 import Tengoku.Data.Set.Finite.Basic
 import Tengoku.Tactic.TypeStar
@@ -1588,5 +1588,31 @@ theorem _root_.Finite.Equation65_implies_Equation817 (G : Type*) [Magma G] [Fini
   have step53 (X0 : G) : ((X0 ◇ X0) ◇ (X0 ◇ X0)) = (X0 ◇ (X0 ◇ X0)) := superpose step13 step47
   have step54 : sK0 ≠ (sK0 ◇ (sK0 ◇ (sK0 ◇ sK0))) := superpose step53 step9
   subsumption step54 step8
+
+theorem _root_.Finite.Equation680_implies_Equation1629 (G : Type*) [Magma G] [Finite G] (h : Equation680 G) : Equation1629 G
+:= by
+  by_contra nh
+  simp only [not_forall] at nh
+  obtain ⟨sK0, nh⟩ := nh
+  have step9 : sK0 ≠ ((sK0 ◇ sK0) ◇ ((sK0 ◇ sK0) ◇ sK0)) := mod_symm nh
+  have step10 (X Y : G) : ((Y ◇ X) ◇ ((Y ◇ Y) ◇ Y)) = X := by
+    let S : Set G := Set.univ
+    have m1 : S.MapsTo (fun s => (s ◇ ((Y ◇ Y) ◇ Y))) S := by
+      intro
+      simp [S]
+    have m2 : S.MapsTo (fun s => (Y ◇ s)) S := by
+      intro
+      simp [S]
+    have linv : S.LeftInvOn (fun s => (Y ◇ s)) (fun s => (s ◇ ((Y ◇ Y) ◇ Y))) := by
+      intro a ha
+      simp [S]
+      simp [← h]
+    have t := linv.surjOn m1
+    rw [Set.Finite.surjOn_iff_bijOn_of_mapsTo (Set.toFinite _) m2] at t
+    have rinv := Set.InjOn.rightInvOn_of_leftInvOn t.injOn linv m2 m1
+    apply rinv _
+    simp [S]
+  have step13 : sK0 ≠ sK0 := superpose step10 step9
+  subsumption step13 rfl
 
 end EquationalTheories
