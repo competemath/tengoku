940188774835980f720bac56dd7b755c30967615
mikaelbashir <bashirm-y16@mgs.org>
2026-09-12 23:11:54 +0100

    Promote equational-theories/Equation1227_implies_Equation100 (#15317)
    equational-theories: promoted 1; not promoted 0 in 0 file(s)

--- Tengoku/EquationalTheories/Generated/MagmaEgg/small/_000.lean
+++ Tengoku/EquationalTheories/Generated/MagmaEgg/small/_000.lean
@@ -1,4 +1,4 @@
--- Tengoku.EquationalTheories.Generated.MagmaEgg.small._000: verified translations of equational_theories/Generated/MagmaEgg/small/_000.lean (40 theorems)
+-- Tengoku.EquationalTheories.Generated.MagmaEgg.small._000: verified translations of equational_theories/Generated/MagmaEgg/small/_000.lean (41 theorems)
 import Tengoku.EquationalTheories.Deps.Magma
 import Tengoku.EquationalTheories.Deps.Equations
 
@@ -208,5 +208,10 @@ theorem Equation2696_implies_Equation203 (G: Type _) [Magma G] (h: Equation2696 
 := fun x =>
   let v0 := M x x
   T (h x (M v0 v0)) (C (C (S (h x x)) (R v0)) (R x))
+
+theorem Equation1227_implies_Equation100 (G: Type _) [Magma G] (h: Equation1227 G) : Equation100 G
+:= fun x y =>
+  let v0 := M x x
+  T (h x (M (M (M v0 v0) x) x) y) (C (R x) (C (S (h v0 x x)) (R y)))
 
 end EquationalTheories
