2ed054b819bdb2e97b5918d2c3eaa4e86b156ff6
mikaelbashir <bashirm-y16@mgs.org>
2026-09-12 18:40:31 +0100

    Stage equational-theories/Finite.Equation481_implies_Equation2041 (#22042)

--- Tengoku/EquationalTheories/Generated/FiniteImplicationSearch/theorems/Inverses1.lean
+++ Tengoku/EquationalTheories/Generated/FiniteImplicationSearch/theorems/Inverses1.lean
@@ -1,4 +1,4 @@
--- Tengoku.EquationalTheories.Generated.FiniteImplicationSearch.theorems.Inverses1: verified translations of equational_theories/Generated/FiniteImplicationSearch/theorems/Inverses1.lean (40 theorems)
+-- Tengoku.EquationalTheories.Generated.FiniteImplicationSearch.theorems.Inverses1: verified translations of equational_theories/Generated/FiniteImplicationSearch/theorems/Inverses1.lean (41 theorems)
 import Tengoku.EquationalTheories.Deps.Superposition
 import Tengoku.Data.Set.Finite.Basic
 import Tengoku.Tactic.TypeStar
@@ -1141,5 +1141,33 @@ theorem _root_.Finite.Equation481_implies_Equation1496 (G : Type*) [Magma G] [Fi
   have step12 (X0 X1 : G) : (X1 ◇ X1) = (X0 ◇ X0) := superpose step8 step8
   have step20 (X0 : G) : sK0 ≠ ((sK1 ◇ sK0) ◇ (sK1 ◇ (X0 ◇ X0))) := superpose step12 step9
   subsumption step20 step10
+
+theorem _root_.Finite.Equation481_implies_Equation2041 (G : Type*) [Magma G] [Finite G] (h : Equation481 G) : Equation2041 G
+:= by
+  by_contra nh
+  simp only [not_forall] at nh
+  obtain ⟨sK0, sK1, nh⟩ := nh
+  have step8 (X0 X1 X2 : G) : (X1 ◇ (X0 ◇ (X1 ◇ (X2 ◇ X2)))) = X0 := mod_symm (h ..)
+  have step9 : sK0 ≠ (((sK0 ◇ sK0) ◇ sK1) ◇ (sK0 ◇ sK1)) := mod_symm nh
+  have step10 (X Y Z : G) : ((Y ◇ X) ◇ (Y ◇ (Z ◇ Z))) = X := by
+    let S : Set G := Set.univ
+    have m1 : S.MapsTo (fun s => (s ◇ (Y ◇ (Z ◇ Z)))) S := by
+      intro
+      simp [S]
+    have m2 : S.MapsTo (fun s => (Y ◇ s)) S := by
+      intro
+      simp [S]
+    have linv : S.LeftInvOn (fun s => (Y ◇ s)) (fun s => (s ◇ (Y ◇ (Z ◇ Z)))) := by
+      intro a ha
+      simp [S]
+      simp [← h]
+    have t := linv.surjOn m1
+    rw [Set.Finite.surjOn_iff_bijOn_of_mapsTo (Set.toFinite _) m2] at t
+    have rinv := Set.InjOn.rightInvOn_of_leftInvOn t.injOn linv m2 m1
+    apply rinv _
+    simp [S]
+  have step38 (X0 X1 X2 : G) : (((X1 ◇ X1) ◇ X0) ◇ (X2 ◇ X0)) = X2 := superpose step10 step8
+  have step78 : sK0 ≠ sK0 := superpose step38 step9
+  subsumption step78 rfl
 
 end EquationalTheories
