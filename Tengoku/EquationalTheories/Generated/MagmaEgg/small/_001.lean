a84a3d4780cec4f82b041f35d6244ff504739b47
mikaelbashir <bashirm-y16@mgs.org>
2026-09-12 23:38:04 +0100

    Promote equational-theories/Equation2343_implies_Equation5 (#15545)
    equational-theories: promoted 1; not promoted 0 in 0 file(s)

--- Tengoku/EquationalTheories/Generated/MagmaEgg/small/_001.lean
+++ Tengoku/EquationalTheories/Generated/MagmaEgg/small/_001.lean
@@ -1,4 +1,4 @@
--- Tengoku.EquationalTheories.Generated.MagmaEgg.small._001: verified translations of equational_theories/Generated/MagmaEgg/small/_001.lean (99 theorems)
+-- Tengoku.EquationalTheories.Generated.MagmaEgg.small._001: verified translations of equational_theories/Generated/MagmaEgg/small/_001.lean (100 theorems)
 import Tengoku.EquationalTheories.Deps.Magma
 import Tengoku.EquationalTheories.Deps.Equations
 
@@ -610,5 +610,12 @@ theorem Equation2282_implies_Equation1459 (G: Type _) [Magma G] (h: Equation2282
   let v0 := M y (M z z)
   have h1 := h x v0 x
   T h1 (C (T (h (M x (M v0 (M x x))) y z) (C (S h1) (R y))) (R v0))
+
+theorem Equation2343_implies_Equation5 (G: Type _) [Magma G] (h: Equation2343 G) : Equation5 G
+:= fun x y =>
+  have h0 := S (h y x x)
+  let v1 := M x (M x (M x x))
+  have h2 := R v1
+  T (h x v1 y) (C (T (C h2 (T (C h2 h0) h0)) h0) (R x))
 
 end EquationalTheories
