101e6a01c15120c513e054c79f4bc3ac55fa63eb
mikaelbashir <bashirm-y16@mgs.org>
2026-09-13 10:29:42 +0100

    Stage equational-theories/Equation2861_implies_Equation2857 (#19569)
    equational-theories: promoted 0; not promoted 0 in 0 file(s); 1 file(s) still being banked

--- Tengoku/EquationalTheories.lean
+++ Tengoku/EquationalTheories.lean
@@ -10,4 +10,5 @@ import Tengoku.EquationalTheories.Generated.MagmaEgg.small._011
 import Tengoku.EquationalTheories.Generated.MagmaEgg.small._016
 import Tengoku.EquationalTheories.Generated.MagmaEgg.small._017
 import Tengoku.EquationalTheories.Generated.SimpleRewrites.theorems.Rewrite_uw
+import Tengoku.EquationalTheories.Generated.SimpleRewrites.theorems.Rewrite_uw_vu
 import Tengoku.EquationalTheories.Generated.SimpleRewrites.theorems.Rewrite_vx
---
+++ Tengoku/EquationalTheories/Generated/SimpleRewrites/theorems/Rewrite_uw_vu.lean
@@ -0,0 +1,53 @@
+-- Tengoku.EquationalTheories.Generated.SimpleRewrites.theorems.Rewrite_uw_vu: verified translations of equational_theories/Generated/SimpleRewrites/theorems/Rewrite_uw_vu.lean (13 theorems)
+import Tengoku.Tactic
+import Tengoku.EquationalTheories.Deps.Magma
+import Tengoku.EquationalTheories.Deps.Equations
+
+set_option linter.all false
+
+namespace EquationalTheories
+
+namespace SimpleRewrites
+
+theorem Equation613_implies_Equation607 (G : Type*) [Magma G] (h : Equation613 G) : Equation607 G
+:= λ x y z w u => h x y z w w u
+
+theorem Equation816_implies_Equation810 (G : Type*) [Magma G] (h : Equation816 G) : Equation810 G
+:= λ x y z w u => h x y z w w u
+
+theorem Equation1019_implies_Equation1013 (G : Type*) [Magma G] (h : Equation1019 G) : Equation1013 G
+:= λ x y z w u => h x y z w w u
+
+theorem Equation1222_implies_Equation1216 (G : Type*) [Magma G] (h : Equation1222 G) : Equation1216 G
+:= λ x y z w u => h x y z w w u
+
+theorem Equation1425_implies_Equation1419 (G : Type*) [Magma G] (h : Equation1425 G) : Equation1419 G
+:= λ x y z w u => h x y z w w u
+
+theorem Equation1628_implies_Equation1622 (G : Type*) [Magma G] (h : Equation1628 G) : Equation1622 G
+:= λ x y z w u => h x y z w w u
+
+theorem Equation2034_implies_Equation2028 (G : Type*) [Magma G] (h : Equation2034 G) : Equation2028 G
+:= λ x y z w u => h x y z w w u
+
+theorem Equation2237_implies_Equation2231 (G : Type*) [Magma G] (h : Equation2237 G) : Equation2231 G
+:= λ x y z w u => h x y z w w u
+
+theorem Equation2643_implies_Equation2637 (G : Type*) [Magma G] (h : Equation2643 G) : Equation2637 G
+:= λ x y z w u => h x y z w w u
+
+theorem Equation2846_implies_Equation2840 (G : Type*) [Magma G] (h : Equation2846 G) : Equation2840 G
+:= λ x y z w u => h x y z w w u
+
+theorem Equation3049_implies_Equation3043 (G : Type*) [Magma G] (h : Equation3049 G) : Equation3043 G
+:= λ x y z w u => h x y z w w u
+
+theorem Equation3252_implies_Equation3246 (G : Type*) [Magma G] (h : Equation3252 G) : Equation3246 G
+:= λ x y z w u => h x y z w w u
+
+theorem Equation3861_implies_Equation3855 (G : Type*) [Magma G] (h : Equation3861 G) : Equation3855 G
+:= λ x y z w u => h x y z w w u
+
+end SimpleRewrites
+
+end EquationalTheories
