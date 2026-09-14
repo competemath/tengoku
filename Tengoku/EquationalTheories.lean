1b9477a232c9c3751bfbfd53a70ec9e3612ebf52
mikaelbashir <bashirm-y16@mgs.org>
2026-09-14 21:37:43 +0100

    Stage equational-theories/Equation522_implies_Equation4216 (#15064)
    equational-theories: promoted 0; not promoted 0 in 0 file(s); 0 file(s) still being banked

--- Tengoku/EquationalTheories.lean
+++ Tengoku/EquationalTheories.lean
@@ -24,6 +24,7 @@ import Tengoku.EquationalTheories.Generated.MagmaEgg.small._009
 import Tengoku.EquationalTheories.Generated.MagmaEgg.small._010
 import Tengoku.EquationalTheories.Generated.MagmaEgg.small._011
 import Tengoku.EquationalTheories.Generated.MagmaEgg.small._012
+import Tengoku.EquationalTheories.Generated.MagmaEgg.small._013
 import Tengoku.EquationalTheories.Generated.MagmaEgg.small._016
 import Tengoku.EquationalTheories.Generated.MagmaEgg.small._017
 import Tengoku.EquationalTheories.Generated.SimpleRewrites.theorems.Rewrite_uw
