4fe0b309f1d2503100812a25c0fc4f6aa7d76897
mikaelbashir <bashirm-y16@mgs.org>
2026-09-14 21:08:58 +0100

    Promote equational-theories/Equation2714_implies_Equation1793 (#15173)
    equational-theories: promoted 100; not promoted 0 in 0 file(s); 0 file(s) still being banked

--- Tengoku/EquationalTheories.lean
+++ Tengoku/EquationalTheories.lean
@@ -13,6 +13,7 @@ import Tengoku.EquationalTheories.Generated.FiniteImplicationSearch.theorems.Inv
 import Tengoku.EquationalTheories.Generated.Greedy.OrLemmas
 import Tengoku.EquationalTheories.Generated.MagmaEgg.small._000
 import Tengoku.EquationalTheories.Generated.MagmaEgg.small._001
+import Tengoku.EquationalTheories.Generated.MagmaEgg.small._002
 import Tengoku.EquationalTheories.Generated.MagmaEgg.small._006
 import Tengoku.EquationalTheories.Generated.MagmaEgg.small._007
 import Tengoku.EquationalTheories.Generated.MagmaEgg.small._010
