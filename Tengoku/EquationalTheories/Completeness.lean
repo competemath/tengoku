e5c2b715ad181054a6a82533302a3615a0e0a420
mikaelbashir <bashirm-y16@mgs.org>
2026-09-22 15:06:25 +0100

    Promote equational-theories: equational-theories: promoted 1; not promoted 0 in 0 file(s); 0 file(s) still being banked
    Signed-off-by: mikaelbashir <bashirm-y16@mgs.org>

--- Tengoku/EquationalTheories/Completeness.lean
+++ Tengoku/EquationalTheories/Completeness.lean
@@ -1,4 +1,5 @@
--- Tengoku.EquationalTheories.Completeness: verified translations of equational_theories/Completeness.lean (1 theorem)
+-- Tengoku.EquationalTheories.Completeness: verified translations of equational_theories/Completeness.lean (2 theorems)
+import Tengoku
 import Tengoku.Data.FunLike.Basic
 import Tengoku.Logic.Equiv.Basic
 import Tengoku.EquationalTheories.Deps.Magma
@@ -12,5 +13,12 @@ namespace EquationalTheories
 
 lemma eq_app : ∀ α β (f g : α → β), f = g → ∀ x, f x = g x
 := fun _ _ _ _ a x ↦ congrFun a x
+
+lemma _root_.Quot.liftEq {α β} [s : Setoid α] (f g : Quotient s → β) (h : f ∘ (⟦.⟧) = g ∘ (⟦.⟧)) :
+    f = g
+:= by
+  refine funext fun x => ?_
+  let ⟨ x_bar, eq_x ⟩ := Quotient.exists_rep x
+  exact eq_x ▸ congrFun h x_bar
 
 end EquationalTheories
